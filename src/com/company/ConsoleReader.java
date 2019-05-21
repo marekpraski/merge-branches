ae19eaceb78e4d3bb01859fc2c9e13a6925f3a7d
Marek Praski <marekpraski@wp.pl>
2019-05-21 21:02:15 +0200

    ConsoleReader on master

---
+++ src/com/company/ConsoleReader.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ConsoleReader {
+    private Scanner scanner;
+    ConsoleReader() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void readUserInput() {
+
+        printMessage("enter something");
+        String userInput=scanner.nextLine();
+        printMessage("you entered:  "+ userInput);
+
+    }
+    private void printMessage(String message) {
+        System.out.println(message);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	ConsoleReader cr = new ConsoleReader();
+	cr.readUserInput();
+    }
+}
