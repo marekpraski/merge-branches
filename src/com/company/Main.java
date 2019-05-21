354279b96079dcfc0ab5f5ac4ac2696b6835606b
Marek Praski <marekpraski@wp.pl>
2019-05-21 21:08:45 +0200

    Marek commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	ConsoleReader cr = new ConsoleReader();
 	cr.readUserInput();
+	Marek mr = new Marek();
+	mr.printMarek();
     }
 }
---
+++ src/com/company/Marek.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Marek {
+    public void printMarek() {
+        System.out.println("Marek marek marek");
+    }
+}
